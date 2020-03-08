15d4c8240c4f621bf8b7841fbacb5fff8669b6f9
Mathias Uhl <m.uhl@ibdigital.de>
2020-03-08 23:34:00 +0100

    final v1

--- app/controllers/finish_controller.rb
+++ app/controllers/finish_controller.rb
@@ -1,2 +1,15 @@
 class FinishController < ApplicationController
+
+  before_action :get_params
+
+  def index
+    @questions_incorrect = ((Question.all.count - session[:score]).to_f * 1.25).round
+  end
+  
+
+  def get_params
+    @parameters = params.permit(params.keys).to_h
+    session[:init] = true
+  end
+
 end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,54 +2,31 @@ class GameController < ApplicationController
   before_action :get_params
 
   def index
-    session[:score] = 0 unless session[:score]
-    
-    case session[:mode]
-      when 'image'
-        if session[:image_id]
-          @image = Image.find(session[:image_id]).next
-        else 
-          @image = Image.first 
-        end
-        @mode = 'image'
-        #redirect_to(@finish_path) if @image.id == Image.all.last.id
-      when 'question'
-        @image = Image.find(session[:image_id])
-        if session[:question_id]
-          if (Question.find(session[:question_id]) == @image.questions.last) || 
-             (!@image.questions.collect(&:id).include?(session[:question_id]))
-            session[:question_id] = nil
-            session[:mode] = "image"
-            else 
-             @question = @image.questions.find(session[:question_id]).next
-          end
-        else 
-          @question = @image.questions.first 
-          if @question 
-            session[:question_id] = @question.id 
-            @mode = 'question'
-          else 
-            session[:question_id] = nil 
-            @mode = 'image'    
-          end
-        end
-      else 
-        session[:image_id] = Image.first.id
-        session[:mode] = "image"
-        @image = Image.first
-    end
-  end
 
-  def image_shown
-    session[:image_id] = @parameters[:id]
-    session[:mode] = "question"
-    redirect_to game_path
+    # check if we need to init the session 
+    if !session[:score]
+      session[:score] = 0
+      session[:open_questions_ids] = Question.all.order(:sorting_prio).collect(&:id)
+      session[:solved_questions_ids] = [] 
+      session[:next_question_id] = session[:open_questions_ids].first
+    end
+     
+    # check if there no questions left
+    if session[:open_questions_ids].empty?
+      redirect_to finish_path
+    else 
+      @question = Question.find(session[:next_question_id])
+      @image = @question.image
+    end
+
   end
   
   def answer_question
     a = Answer.find(@parameters[:id])
     session[:score] =session[:score]+1  if a.correct
-    session[:mode] = "image" if a.question.answers.last.id == a.id 
+    session[:solved_questions_ids] << a.id
+    session[:open_questions_ids]= session[:open_questions_ids].drop(1)
+    session[:next_question_id] = session[:open_questions_ids].first
     redirect_to game_path
   end
 
